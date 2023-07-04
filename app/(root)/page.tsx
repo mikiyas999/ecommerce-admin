6f8b49a5cfc9a982b520f5adfd4f8eee00ae3676
mikiyas <mikiyasget1234@gmail.com>
2023-07-04 14:35:54 +0100

    create env file

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <Button size="default" variant="ghost">
+        Click me
+      </Button>
+    </div>
+  );
+}
