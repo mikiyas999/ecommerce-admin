93d0728b9e9cd4bd1daf44df02441ad9ab3a39ae
mikiyas <mikiyasget1234@gmail.com>
2023-07-06 15:06:08 +0100

    setup prisma client

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,25 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+
+const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
-    <div className="p-4">
-      <Button size="default" variant="ghost">
-        Click me
-      </Button>
+    <div>
+      <p>root page</p>
     </div>
   );
-}
+};
+
+export default SetupPage;
