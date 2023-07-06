79948719d2ff3cd7a01a5e9cad0f94363bd2f625
mikiyas <mikiyasget1234@gmail.com>
2023-07-06 20:19:32 +0100

    implement navbar

---
+++ components/main-nav.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation";
+
+const MainNav = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) => {
+  const params = useParams();
+  const pathname = usePathname();
+
+  const routes = [
+    {
+      href: `/${params.storeId}/setting`,
+      label: "Setting",
+      active: pathname === `/${params.storeId}/setting`,
+    },
+  ];
+
+  return (
+    <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
+      {routes.map((route) => (
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary ",
+            route.active
+              ? "text-black dark:text-white"
+              : "text-muted-foreground"
+          )}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default MainNav;
