e8f421d840905e641119288d261b32c263eb9aca
mikiyas <mikiyasget1234@gmail.com>
2023-07-08 13:53:39 +0100

    cloudinary-upload

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -56,7 +56,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="xzalag1a">
         {({ open }) => {
           const onClick = () => {
             open();
